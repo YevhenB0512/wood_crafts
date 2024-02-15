a55ec57078f6200b5c66852d1cc1efe9ca350f50
YevhenB0512 <evgenijboychuk@gmail.com>
2024-02-15 21:48:19 +0300

    create project wood_crafts store and app main

---
+++ main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        'title': 'Wood crafts - Главная',
+        'content': 'Wood crafts store'
+    }
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'Wood crafts - О нас',
+    }
+    return render(request, 'main/about.html', context)
