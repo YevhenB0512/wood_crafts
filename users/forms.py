befcbc387ba4fa9fa2a6a2d3712981b848a343f0
YevhenB0512 <evgenijboychuk@gmail.com>
2024-02-20 00:09:58 +0300

    added profile form and controller for profile

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from users.models import User
 from django import forms
 
@@ -27,3 +27,22 @@ class UserRegistrationForm(UserCreationForm):
     email = forms.CharField()
     password1 = forms.CharField()
     password2 = forms.CharField()
+
+
+class UserProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = (
+            'image',
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+        )
+
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
