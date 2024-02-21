6a1e9df2dc542cd83e58e1bc8d3d50aedd554d3d
YevhenB0512 <evgenijboychuk@gmail.com>
2024-02-22 00:03:28 +0300

    creating order app

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Фото профиля')
+    phone_number = models.CharField(max_length=20, blank=True, null=True, verbose_name="Номер телефона")
 
     class Meta:
         db_table = 'user'
--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
+from django.db.models import Prefetch
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from carts.models import Cart
+from orders.models import Order, OrderItem
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
@@ -72,9 +74,16 @@ def profile(request):
             return redirect(reverse('user:profile'))
     else:
         form = UserProfileForm(instance=request.user)
+        orders = Order.objects.filter(user=request.user).prefetch_related(
+            Prefetch(
+                "orderitem_set",
+                queryset=OrderItem.objects.select_related("product"),
+            )
+        ).order_by("-id")
     context = {
         'title': 'Wood crafts - Кабинет',
-        'form': form
+        'form': form,
+        'orders': orders,
     }
     return render(request, 'users/profile.html', context)
 
